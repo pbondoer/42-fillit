57c634caeeb12db9388cc5b269cb0a75549e6d2e
Pierre Bondoerffer <pbondoer@student.42.fr>
2016-01-27 20:50:42 +0100

    basic error checking

--- src/reader.c
+++ src/reader.c
@@ -6,13 +6,37 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 17:43:27 by pbondoer          #+#    #+#             */
-/*   Updated: 2016/01/27 18:21:52 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/01/27 20:50:32 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
+int		check_counts(char *str, int count)
+{
+	int i;
+	int blocs;
+
+	blocs = 0;
+	i = 0;
+	while (i < 20)
+	{
+		if (i % 5 < 4)
+		{
+			if (!(str[i] == '#' || str[i] == '.'))
+				return (1);
+			if (str[i] == '#' && blocs++ > 4)
+				return (2);
+		}
+		else if (str[i] != '\n')
+			return (3);
+		i++;
+	}
+	if (count == 21 && str[20] != '\n')
+		return (4);
+	return (0);
+}
 
 /*
 ** Read tetriminos from fd and put them in a list.
@@ -26,17 +50,19 @@ t_list	*read_tetri(int fd)
 	char	*buf;
 	int		count;
 	t_list	*list;
+	t_etris	*tetris;
 
 	buf = ft_strnew(21);
 	list = NULL;
 	while ((count = read(fd, buf, 21)) >= 20)
 	{
-		write(1, buf, count);
+		if (check_counts(buf, count))
+			return (NULL);
+		if (build_piece(buf, tetris))
+			return (NULL);
+		ft_lstadd(&list, ft_lstnew(tetris, sizeof(tetris)));
 	}
 	if (count != 0)
-	{
-		//TODO: Cleanup
 		return (NULL);
-	}
 	return (list);
 }

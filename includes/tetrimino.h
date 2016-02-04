c16d957e314c41f8c0581613153fc17970eb1df5
Pierre Bondoerffer <pbondoer@student.42.fr>
2016-02-04 15:05:13 +0100

    change memory scheme

--- includes/tetrimino.h
+++ includes/tetrimino.h
@@ -6,21 +6,17 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 17:58:37 by pbondoer          #+#    #+#             */
-/*   Updated: 2016/01/27 18:11:58 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/02/04 15:05:01 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TETRIMINO_H
 # define TETRIMINO_H
 
-typedef struct	s_vector
-{
-	char		x;
-	char		y;
-}				t_vector;
 typedef struct	s_etris
 {
-	t_vector	*pos;
-	t_vector	size;
+	char		*pos;
+	char		w;
+	char		h;
 }				t_etris;
 #endif
--- src/tetrimino.c
+++ src/tetrimino.c
@@ -6,26 +6,17 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 18:06:28 by pbondoer          #+#    #+#             */
-/*   Updated: 2016/01/27 18:17:30 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/02/04 15:04:22 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_etris		tetris_new(t_vector pos, t_vector size)
+t_etris		tetris_new(char *pos, char width, char height)
 {
 	t_etris		*tetris;
 
-	tetris = ft_memalloc(sizeof(t_etris));
+	tetris = ft_memalloc(sizeof(char) * width * height);
 	tetris->pos = pos;
-	tetris->size = size;
+	tetris->w = width;
+	tetris->h = height;
 	return (tetris);
-}
-
-t_vector	*vector_new(char x, char y)
-{
-	t_vector	*vector;
-
-	vector = ft_memalloc(sizeof(t_vector));
-	vector->x = x;
-	vector->y = y;
-	return (vector);
 }
